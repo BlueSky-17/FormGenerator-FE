da0d7d684250d8e8fbea892d69b8778bf83082a6
Quang Thành <thanh.dangquang28@gmail.com>
2023-09-21 23:49:22 +0700

    add Popup to edit detail form

--- client/src/pages/home/DetailForm/index.tsx
+++ client/src/pages/home/DetailForm/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Typography, Drawer, Avatar, IconButton, Toolbar, List, Divider, Icon } from '@mui/material'
+import { Box, Typography, Drawer, Avatar, IconButton, Toolbar, List, Divider, Icon, Modal } from '@mui/material'
 import { styled, useTheme, alpha } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 
@@ -16,6 +16,17 @@ import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
 import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
 import DeleteIcon from '@mui/icons-material/Delete';
+
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
 
 const DrawerHeader = styled('div')(({ theme }) => ({
     display: 'flex',
@@ -44,7 +55,30 @@ const rows = [
     createData('Ảnh', 'Ảnh', '4x6 cm', false, true),
 ];
 
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '35%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 700,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    borderRadius: '15px',
+    boxShadow: 24,
+    p: 4,
+};
+
 function DetailForm() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const [type, setType] = React.useState('');
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setType(event.target.value as string);
+    };
+
     return (
         <div>
             <DrawerHeader />
@@ -84,7 +118,7 @@ function DetailForm() {
                 </Box>
 
                 <Divider />
-                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '5px' }}>
+                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '5px', borderBottom: "10px solid #364F6B" }}>
                     <Button sx={{
                         color: 'white',
                         border: '2px solid #364F6B',
@@ -114,28 +148,82 @@ function DetailForm() {
                     </Button>
                 </Box>
 
-                <Box sx={{height: '10px', backgroundColor: '#364F6B'}}>
-                    
-                </Box>
-
-                <Box sx={{display: 'flex', alignContent:'center', margin: '15px'}}>
-                    <Box sx={{display: 'flex', alignContent:'center', flexDirection: 'column'}}>
+                <Box sx={{ display: 'flex', alignContent: 'center', margin: '15px' }}>
+                    <Box sx={{ display: 'flex', alignContent: 'center', flexDirection: 'column' }}>
                         <Typography variant='h4' component="div">Tiêu đề biểu mẫu</Typography>
                         <Typography variant='inherit' component="div">Mô tả biểu mẫu</Typography>
                     </Box>
                     <Box sx={{ flexGrow: 1 }} />
 
-                    <Button sx={{
-                        color: 'white',
-                        backgroundColor: '#176B87',
-                        borderRadius: '50px',
-                        margin: '10px',
-                        '&:hover': {
-                            backgroundColor: '#2E4155', // Màu nền thay đổi khi hover
-                        },
-                    }}>
+                    <Button
+                        onClick={handleOpen}
+                        sx={{
+                            color: 'white',
+                            backgroundColor: '#364F6B',
+                            borderRadius: '50px',
+                            margin: '10px',
+                            '&:hover': {
+                                backgroundColor: '#2E4155', // Màu nền thay đổi khi hover
+                            },
+                        }}>
                         Chỉnh sửa
                     </Button>
+
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="parent-modal-title"
+                        aria-describedby="parent-modal-description"
+                    >
+                        <Box sx={{ ...style, width: 400 }}>
+                            <Modal
+                                open={open}
+                                onClose={handleClose}
+                                aria-labelledby="modal-modal-title"
+                                aria-describedby="modal-modal-description"
+                            >
+                                <Box sx={style}>
+                                    <Typography variant='h6' component="div">
+                                        Chỉnh sửa câu hỏi
+                                    </Typography>
+
+                                    <Box sx={{ marginY: '10px', display: 'flex', alignItems: 'center' }}>
+                                        <TextField sx={{ marginRight: '10px', width: '100%' }} id="outlined-basic" variant="outlined" placeholder='Nhập nội dung câu hỏi...' />
+                                        <FormControl fullWidth>
+                                            <InputLabel id="demo-simple-select-label">Dạng</InputLabel>
+                                            <Select
+                                                labelId="demo-simple-select-label"
+                                                id="demo-simple-select"
+                                                value={type}
+                                                label="Dạng"
+                                                onChange={handleChange}
+                                            >
+                                                <MenuItem value={'Trắc nghiệm'}>Trắc nghiệm</MenuItem>
+                                                <MenuItem value={'Lựa chọn'}>Lựa chọn</MenuItem>
+                                                <MenuItem value={'Điền ngắn'}>Điền ngắn</MenuItem>
+                                            </Select>
+                                        </FormControl>
+                                    </Box>
+                                    <FormControl>
+                                        {/* <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel> */}
+                                        <RadioGroup
+                                            aria-labelledby="demo-radio-buttons-group-label"
+                                            defaultValue="female"
+                                            name="radio-buttons-group"
+                                        >
+                                            <FormControlLabel value="female" control={<Radio />} label="Nam" />
+                                            <FormControlLabel value="male" control={<Radio />} label="Nữ" />
+                                            <FormControlLabel value="other" control={<Radio />} label="Khác" />
+                                        </RadioGroup>
+                                    </FormControl>
+                                    <Box sx={{ display: 'flex', flexDirection: 'row-reverse' }} >
+                                        <Button onClick={handleClose}>Lưu</Button>
+                                        <Button onClick={handleClose}>Hủy</Button>
+                                    </Box>
+                                </Box>
+                            </Modal>
+                        </Box>
+                    </Modal>
                 </Box>
 
                 <Divider />
@@ -172,9 +260,9 @@ function DetailForm() {
                                                     backgroundColor: '#176B87', // Màu nền thay đổi khi hover
                                                 },
                                             }}>
-                                                <EditIcon/>
-                                           </IconButton>
-                                           <IconButton sx={{
+                                                <EditIcon />
+                                            </IconButton>
+                                            <IconButton sx={{
                                                 backgroundColor: '#364F6B',
                                                 color: 'white',
                                                 margin: '5px',
@@ -182,9 +270,9 @@ function DetailForm() {
                                                     backgroundColor: '#176B87', // Màu nền thay đổi khi hover
                                                 },
                                             }}>
-                                                <ContentCopyIcon/>
-                                           </IconButton>
-                                           <IconButton sx={{
+                                                <ContentCopyIcon />
+                                            </IconButton>
+                                            <IconButton sx={{
                                                 backgroundColor: '#364F6B',
                                                 color: 'white',
                                                 margin: '5px',
@@ -192,9 +280,9 @@ function DetailForm() {
                                                     backgroundColor: '#176B87', // Màu nền thay đổi khi hover
                                                 },
                                             }}>
-                                                <ArrowCircleDownIcon/>
-                                           </IconButton>
-                                           <IconButton sx={{
+                                                <ArrowCircleDownIcon />
+                                            </IconButton>
+                                            <IconButton sx={{
                                                 backgroundColor: '#364F6B',
                                                 color: 'white',
                                                 margin: '5px',
@@ -202,9 +290,9 @@ function DetailForm() {
                                                     backgroundColor: '#176B87', // Màu nền thay đổi khi hover
                                                 },
                                             }}>
-                                                <ArrowCircleUpIcon/>
-                                           </IconButton>
-                                           <IconButton sx={{
+                                                <ArrowCircleUpIcon />
+                                            </IconButton>
+                                            <IconButton sx={{
                                                 backgroundColor: '#364F6B',
                                                 color: 'white',
                                                 margin: '5px',
@@ -212,14 +300,30 @@ function DetailForm() {
                                                     backgroundColor: '#176B87', // Màu nền thay đổi khi hover
                                                 },
                                             }}>
-                                                <DeleteIcon/>
-                                           </IconButton>
+                                                <DeleteIcon />
+                                            </IconButton>
                                         </TableCell>
                                     </TableRow>
                                 ))}
                             </TableBody>
                         </Table>
                     </TableContainer>
+                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                        <Button
+                            onClick={handleOpen}
+                            sx={{
+                                color: 'white',
+                                backgroundColor: '#364F6B',
+                                borderRadius: '20px',
+                                paddingX: '20px',
+                                margin: '15px',
+                                '&:hover': {
+                                    backgroundColor: '#2E4155', // Màu nền thay đổi khi hover
+                                },
+                            }}>
+                            Thêm mới
+                        </Button>
+                    </Box>
                 </Box>
             </Box>
         </div>
