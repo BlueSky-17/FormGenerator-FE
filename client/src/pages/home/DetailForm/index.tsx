3ce7fc596758b75cdcbe1a466299338949b550a6
Quang Thành <thanh.dangquang28@gmail.com>
2023-09-24 17:44:10 +0700

    update popupEdit

--- client/src/pages/home/DetailForm/index.tsx
+++ client/src/pages/home/DetailForm/index.tsx
@@ -19,6 +19,9 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import PaletteIcon from '@mui/icons-material/Palette';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import NotesIcon from '@mui/icons-material/Notes';
 
 import TextField from '@mui/material/TextField';
 import InputLabel from '@mui/material/InputLabel';
@@ -30,6 +33,7 @@ import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormLabel from '@mui/material/FormLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 const DrawerHeader = styled('div')(({ theme }) => ({
     display: 'flex',
@@ -98,7 +102,7 @@ function DetailForm() {
                         borderRadius: '10px',
                         fontSize: '0.8rem',
                         marginY: '18px',
-                        textTransform:'initial',
+                        textTransform: 'initial',
                         '&:hover': {
                             backgroundColor: '#364F6B', // Màu nền thay đổi khi hover
                             color: 'white'
@@ -116,7 +120,7 @@ function DetailForm() {
                         marginY: '18px',
                         marginX: '15px',
                         paddingX: '10px',
-                        textTransform:'initial',
+                        textTransform: 'initial',
                         '&:hover': {
                             backgroundColor: '#364F6B', // Màu nền thay đổi khi hover
                             color: 'white'
@@ -208,27 +212,78 @@ function DetailForm() {
                                                 label="Dạng"
                                                 onChange={handleChange}
                                             >
-                                                <MenuItem value={'Trắc nghiệm'}>Trắc nghiệm</MenuItem>
-                                                <MenuItem value={'Lựa chọn'}>Lựa chọn</MenuItem>
-                                                <MenuItem value={'Điền ngắn'}>Điền ngắn</MenuItem>
+                                                <MenuItem value={'Trắc nghiệm'}>
+                                                    <RadioButtonCheckedIcon sx={{ paddingRight: '5px' }} />
+                                                    Trắc nghiệm
+                                                </MenuItem>
+                                                <MenuItem value={'Ô đánh dấu'}>
+                                                    <CheckBoxIcon sx={{ paddingRight: '5px' }} />
+                                                    Ô đánh dấu
+                                                </MenuItem>
+                                                <MenuItem value={'Điền ngắn'}>
+                                                    <NotesIcon sx={{ paddingRight: '5px' }} />
+                                                    Điền ngắn
+                                                </MenuItem>
                                             </Select>
                                         </FormControl>
                                     </Box>
-                                    <FormControl>
-                                        {/* <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel> */}
-                                        <RadioGroup
-                                            aria-labelledby="demo-radio-buttons-group-label"
-                                            defaultValue="female"
-                                            name="radio-buttons-group"
-                                        >
-                                            <FormControlLabel value="female" control={<Radio />} label="Nam" />
-                                            <FormControlLabel value="male" control={<Radio />} label="Nữ" />
-                                            <FormControlLabel value="other" control={<Radio />} label="Khác" />
-                                        </RadioGroup>
-                                    </FormControl>
+                                    {type === 'Trắc nghiệm' ?
+                                        <FormControl>
+                                            {/* <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel> */}
+                                            <RadioGroup
+                                                aria-labelledby="demo-radio-buttons-group-label"
+                                                defaultValue="female"
+                                                name="radio-buttons-group"
+                                            >
+                                                <FormControlLabel value="female" control={<Radio />} label="Nam" />
+                                                <FormControlLabel value="male" control={<Radio />} label="Nữ" />
+                                                <FormControlLabel value="other" control={<Radio />} label="Khác" />
+                                            </RadioGroup>
+                                        </FormControl>
+                                        : null
+                                    }
+                                    {type === 'Ô đánh dấu' ?
+                                        <FormControl>
+                                            {/* <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel> */}
+                                            <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                                            <FormControlLabel required control={<Checkbox />} label="Required" />
+                                            <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
+                                        </FormControl>
+                                        : null
+                                    }
+                                    {type === 'Điền ngắn' ?
+                                        <TextField sx={{ width: '100%' }} id="standard-basic" label="Điền ngắn" variant="standard" />
+                                        : null
+                                    }
                                     <Box sx={{ display: 'flex', flexDirection: 'row-reverse' }} >
-                                        <Button onClick={handleClose}>Lưu</Button>
-                                        <Button onClick={handleClose}>Hủy</Button>
+                                        <Button
+                                            onClick={handleClose}
+                                            sx={{
+                                                color: 'white',
+                                                backgroundColor: '#364F6B',
+                                                borderRadius: '10px',
+                                                marginY: '10px',
+                                                marginX: '5px',
+                                                '&:hover': {
+                                                    backgroundColor: '#2E4155', // Màu nền thay đổi khi hover
+                                                },
+                                            }}>
+                                            Lưu
+                                        </Button>
+                                        <Button
+                                            onClick={handleClose}
+                                            sx={{
+                                                color: '#000000',
+                                                backgroundColor: '#E7E7E8',
+                                                borderRadius: '10px',
+                                                marginY: '10px',
+                                                marginX: '5px',
+                                                '&:hover': {
+                                                    backgroundColor: '#E7E7E7', // Màu nền thay đổi khi hover
+                                                },
+                                            }}>
+                                            Hủy
+                                        </Button>
                                     </Box>
                                 </Box>
                             </Modal>
@@ -242,11 +297,11 @@ function DetailForm() {
                         <Table sx={{ minWidth: 650 }} aria-label="simple table">
                             <TableHead>
                                 <TableRow>
-                                    <TableCell sx={{ fontWeight: 800, fontSize: '1rem' }}  align="left">STT</TableCell>
-                                    <TableCell sx={{ fontWeight: 800, fontSize: '1rem' }}  align="left">Tiêu đề</TableCell>
-                                    <TableCell sx={{ fontWeight: 800, fontSize: '1rem' }}  align="left">Dạng</TableCell>
-                                    <TableCell sx={{ fontWeight: 800, fontSize: '1rem' }}  align="left">Ghi chú</TableCell>
-                                    <TableCell sx={{ fontWeight: 800, fontSize: '1rem' }}  align="center">Chỉnh sửa</TableCell>
+                                    <TableCell sx={{ fontWeight: 800, fontSize: '1rem' }} align="left">STT</TableCell>
+                                    <TableCell sx={{ fontWeight: 800, fontSize: '1rem' }} align="left">Tiêu đề</TableCell>
+                                    <TableCell sx={{ fontWeight: 800, fontSize: '1rem' }} align="left">Dạng</TableCell>
+                                    <TableCell sx={{ fontWeight: 800, fontSize: '1rem' }} align="left">Ghi chú</TableCell>
+                                    <TableCell sx={{ fontWeight: 800, fontSize: '1rem' }} align="center">Chỉnh sửa</TableCell>
                                 </TableRow >
                             </TableHead>
                             <TableBody>
@@ -331,7 +386,7 @@ function DetailForm() {
                                     backgroundColor: '#2E4155', // Màu nền thay đổi khi hover
                                 },
                             }}>
-                                <AddCircleOutlineIcon sx={{marginRight:'8px'}}/>
+                            <AddCircleOutlineIcon sx={{ marginRight: '8px' }} />
                             Thêm mới
                         </Button>
                     </Box>
