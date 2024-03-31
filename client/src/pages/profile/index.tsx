12d1ac48eb81d520b20296a1510bf02d1508d560
Quang Thành <thanh.dangquang28@gmail.com>
2024-04-01 01:08:31 +0700

    add UI profile

--- client/src/pages/profile/index.tsx
+++ client/src/pages/profile/index.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { Box, Typography, TextField, Drawer, Avatar, IconButton, Toolbar, List, Divider, Icon, Modal } from '@mui/material'
 import { styled, useTheme, alpha } from '@mui/material/styles';
 import Button from '@mui/material/Button';
+import COLORS from '../../constants/colors';
 
 const DrawerHeader = styled('div')(({ theme }) => ({
     display: 'flex',
@@ -14,15 +15,66 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 }));
 
 function Profile() {
+    const [profile, setProfile] = useState<any>({})
+
+    const ProfileAPI_URL = `http://localhost:8080/profile`;
+
+    useEffect(() => {
+        setProfile(JSON.parse(sessionStorage.getItem('token') as string)?.user)
+    }, [])
+
+    console.log(profile);
+
     return (
         <Box>
             <DrawerHeader />
-            <Box sx={{ backgroundColor: 'white', border: "2px solid #DEDEDE", paddingTop:'5px' }}>
+            <Box sx={{ backgroundColor: 'white', border: "2px solid #DEDEDE", paddingTop: '5px' }}>
                 <Typography sx={{ color: '#364F6B', padding: '15px', fontWeight: 600 }} variant="h6" noWrap component="div">
                     THÔNG TIN CÁ NHÂN
                 </Typography>
                 <Divider />
-                <Box sx={{ minHeight: 550 }}></Box>
+                <Box sx={{ minHeight: 550, display: 'flex', flexDirection: 'row' }}>
+                    <Box sx={{ height: 'full', display: 'flex', alignItems: 'center', justifyContent: 'center', paddingX: '100px', borderRight: '1px solid #DEDEDE' }}>
+                        <Avatar alt="Avatar" src={profile.AvatarPath} sx={{ width: 200, height: 200 }} />
+                    </Box>
+                    <Box sx={{ padding: '30px', display: 'flex', flexDirection: 'column', width: '100%' }}>
+                        <Box sx={{ marginBottom: '10px' }}>
+                            <Typography variant="h6" gutterBottom sx={{ color: COLORS.darkBlue }}>Họ và tên</Typography>
+                            <TextField
+                                disabled
+                                fullWidth
+                                variant="filled"
+                                id="outlined-disabled"
+                                // label="Disabled"
+                                value={profile.FirstName}
+                            />
+                        </Box>
+
+                        <Box sx={{ marginBottom: '10px' }}>
+                            <Typography variant="h6" gutterBottom sx={{ color: COLORS.darkBlue }}>Email</Typography>
+                            <TextField
+                                disabled
+                                fullWidth
+                                variant="filled"
+                                id="outlined-disabled"
+                                // label="Disabled"
+                                value={profile.Email}
+                            />
+                        </Box>
+
+                        <Box sx={{ marginBottom: '10px' }}>
+                            <Typography variant="h6" gutterBottom sx={{ color: COLORS.darkBlue }}>Vai trò</Typography>
+                            <TextField
+                                disabled
+                                fullWidth
+                                variant="filled"
+                                id="outlined-disabled"
+                                // label="Disabled"
+                                value={profile.Role === 'user' ? 'Người dùng' : 'Quản trị viên'}
+                            />
+                        </Box>
+                    </Box>
+                </Box>
             </Box>
         </Box>
     )
